baeb12cd5b91ce4d32c6e0f1c74be2f2d5a18709
Rileywave <rileyfreeman.xo@gmail.com>
2026-07-16 12:13:18 +0100

    commit

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -14,7 +14,7 @@ const Support = () => {
   };
 
   const handleWhatsAppSupport = () => {
-    window.open("https://wa.me/2349058227746", "_blank");
+    window.open("https://wa.me/2348051880925", "_blank");
   };
 
   const handleEmailSupport = () => {
